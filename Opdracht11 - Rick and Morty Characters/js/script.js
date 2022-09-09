a587e2d078abf559b440f364a70627f5f487aed6
Recep Yildiz <recep.yildiz@student.rocva.nl>
2022-09-09 21:37:30 +0200

    opdracht 11 gestart

--- Opdracht11 - Rick and Morty Characters/js/script.js
+++ Opdracht11 - Rick and Morty Characters/js/script.js
@@ -10,12 +10,12 @@ for (let i = 0; i < characters.length-1; i++) {
 
 for (let i = 0; i < characters.length; i++) {
     character = document.getElementsByClassName("character")[i];
-    character.childNodes[1].innerHTML = characters[i].id;
+    character.childNodes[1].innerHTML = characters[i].name;
     character.childNodes[3].src = characters[i].image;
     character.childNodes[5].childNodes[3].innerHTML = characters[i].status;
-    character.childNodes[5].childNodes[6].innerHTML = characters[i].species;
+    character.childNodes[5].childNodes[3].innerHTML = characters[i].species;
     character.childNodes[5].childNodes[11].innerHTML = characters[i].gender;
-    character.childNodes[5].childNodes[15].innerHTML = characters[i].origin;
+    character.childNodes[5].childNodes[12].innerHTML = characters[i].origin;
 }
 
 exampleCharacter = document.getElementsByClassName("character")[0];
